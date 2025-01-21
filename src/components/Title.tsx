04e0d342b441614516d071af2e013c8af5dd3275
ChriisLM <christianlezcano878@gmail.com>
2025-01-20 21:19:45 -0500

    feat: Added reusable title component

---
+++ src/components/Title.tsx
@@ -0,0 +1,18 @@
+interface TitleProps {
+  title: string;
+  className?: string;
+}
+
+export function Title({ title, className }: TitleProps) {
+  return (
+    <div className="p-2">
+      <h1
+        className={
+          className ? `${className}` : "text-3xl font-bold text-gray-800"
+        }
+      >
+        {title}
+      </h1>
+    </div>
+  );
+}
