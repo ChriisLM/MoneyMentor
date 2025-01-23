d99ead3bfacbacab8280077dac147f98cac5247e
ChriisLM <christianlezcano878@gmail.com>
2025-01-22 20:18:15 -0500

    feat: Added circle add icon

--- src/icons/Icons.tsx
+++ src/icons/Icons.tsx
@@ -114,9 +114,9 @@ export function ChartUpIcon(props: Props) {
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
-      stroke-width="2"
-      stroke-linecap="round"
-      stroke-linejoin="round"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
       className="icon icon-tabler icons-tabler-outline icon-tabler-chart-bar-popular"
       {...props}
     >
@@ -125,6 +125,27 @@ export function ChartUpIcon(props: Props) {
       <path d="M9 9a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v10a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" />
       <path d="M15 5a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v14a1 1 0 0 1 -1 1h-4a1 1 0 0 1 -1 -1z" />
       <path d="M4 20h14" />
+    </svg>
+  );
+}
+
+export function AddCircleIcon(props: Props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      className="icon icon-tabler icons-tabler-outline icon-tabler-circle-plus"
+      {...props}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" />
+      <path d="M3 12a9 9 0 1 0 18 0 9 9 0 0 0-18 0M9 12h6M12 9v6" />
     </svg>
   );
 }
