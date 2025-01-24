6ca289b248614eecdb82bfe682f3639106687c18
ChriisLM <christianlezcano878@gmail.com>
2025-01-24 16:30:23 -0500

    fix: Fixed add viewbox for icons

--- src/icons/SideBarIcons.tsx
+++ src/icons/SideBarIcons.tsx
@@ -9,6 +9,7 @@ export function HomeIcon(props: Props) {
       xmlns="http://www.w3.org/2000/svg"
       width={24}
       height={24}
+      viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
       strokeLinecap="round"
@@ -30,6 +31,7 @@ export function TransactionIcon(props: Props) {
       xmlns="http://www.w3.org/2000/svg"
       width={24}
       height={24}
+      viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
       strokeLinecap="round"
@@ -50,6 +52,7 @@ export function CoinIcon(props: Props) {
       xmlns="http://www.w3.org/2000/svg"
       width={24}
       height={24}
+      viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
       strokeLinecap="round"
@@ -71,6 +74,7 @@ export function GoalIcon(props: Props) {
       xmlns="http://www.w3.org/2000/svg"
       width={24}
       height={24}
+      viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
       strokeLinecap="round"
@@ -88,23 +92,24 @@ export function GoalIcon(props: Props) {
   );
 }
 
-export function InvestmentIcon(props: Props) {
+export function PigCoinIcon(props: Props) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width={24}
       height={24}
+      viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
       strokeLinecap="round"
       strokeLinejoin="round"
       strokeWidth={2}
-      className="icon icon-tabler icons-tabler-outline icon-tabler-trending-up"
+      className="icon icon-tabler icons-tabler-outline icon-tabler-pig-money"
       {...props}
     >
       <path stroke="none" d="M0 0h24v24H0z" />
-      <path d="m3 17 6-6 4 4 8-8" />
-      <path d="M14 7h7v7" />
+      <path d="M15 11v.01M5.173 8.378a3 3 0 1 1 4.656-1.377" />
+      <path d="M16 4v3.803A6.019 6.019 0 0 1 18.658 11h1.341a1 1 0 0 1 1 1v2a1 1 0 0 1-1 1h-1.342c-.336.95-.907 1.8-1.658 2.473V19.5a1.5 1.5 0 0 1-3 0v-.583a6.04 6.04 0 0 1-1 .083h-4a6.04 6.04 0 0 1-1-.083v.583a1.5 1.5 0 0 1-3 0v-2.027A6 6 0 0 1 8.999 7h2.5l4.5-3h0z" />
     </svg>
   );
 }
@@ -115,6 +120,7 @@ export function BarChartIcon(props: Props) {
       xmlns="http://www.w3.org/2000/svg"
       width={24}
       height={24}
+      viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
       strokeLinecap="round"
@@ -135,6 +141,7 @@ export function SelectorIcon(props: Props) {
       xmlns="http://www.w3.org/2000/svg"
       width={24}
       height={24}
+      viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
       strokeLinecap="round"
@@ -157,6 +164,7 @@ export function SettingsIcon(props: Props) {
       xmlns="http://www.w3.org/2000/svg"
       width={24}
       height={24}
+      viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
       strokeLinecap="round"
