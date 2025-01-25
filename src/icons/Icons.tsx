965fb4a6b2e766ceee8f653e38553104f767bab5
ChriisLM <christianlezcano878@gmail.com>
2025-01-25 12:07:18 -0500

    feat: Added Cash Icon

--- src/icons/Icons.tsx
+++ src/icons/Icons.tsx
@@ -171,3 +171,25 @@ export function AmbulenceIcon(props: Props) {
     </svg>
   );
 }
+
+export function CashIcon(props: Props) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth={2}
+      className="icon icon-tabler icons-tabler-outline icon-tabler-cash"
+      {...props}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" />
+      <path d="M7 11a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v6a2 2 0 0 1-2 2H9a2 2 0 0 1-2-2z" />
+      <path d="M12 14a2 2 0 1 0 4 0 2 2 0 1 0-4 0M17 9V7a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h2" />
+    </svg>
+  );
+}
