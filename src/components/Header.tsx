81d5140128cc694678a15400f97b870ae50854ab
ChriisLM <christianlezcano878@gmail.com>
2025-01-28 20:50:16 -0500

    feat: Added theme switching functionality to Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,26 @@
 import { Link, NavLink } from "react-router";
 import { BellIcon, DarkIcon, MenuIcon } from "../icons/Icons";
+import { useEffect, useState } from "react";
 
 export function Header() {
+  const [theme, setTheme] = useState(() => {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+      return "dark";
+    }
+    return "light";
+  });
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.querySelector("html")?.classList.add("dark");
+    } else {
+      document.querySelector("html")?.classList.remove("dark");
+    }
+  }, [theme]);
+
+  const handleChangeTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
   return (
     <header className="bg-white shadow-sm z-10">
       <div className="max-w-8xl px-4 sm:px-6 lg:px-8">
@@ -15,11 +34,6 @@ export function Header() {
             </NavLink>
           </div>
           <div className="flex items-center space-x-4">
-            {/* <select name="" id="">
-              <option value="cop">COP</option>
-              <option value="usd">USD</option>
-              <option value="eur">EUR</option>
-            </select> */}
             <Link
               to={"/help"}
               className="text-gray-900 font-medium hover:text-blue-500 hover:underline"
@@ -30,7 +44,7 @@ export function Header() {
               <BellIcon className="h-6 w-6" />
             </span>
             <span className="bg-transparent rounded-md p-1 hover:bg-gray-100 cursor-pointer">
-              <DarkIcon className="h-6 w-6" />
+              <DarkIcon className="h-6 w-6" onClick={handleChangeTheme} />
             </span>
           </div>
         </div>
