b956ea83e027aa9d8cd5fc352533180aefc6d57a
ChriisLM <christianlezcano878@gmail.com>
2025-02-05 21:37:16 -0500

    feat: Create custom hook for handle data of aplication

---
+++ src/hooks/useFinanceData.ts
@@ -0,0 +1,31 @@
+import { useFinanceStore } from "../stores/useFinanceStore";
+import {
+  calculateTotalBalance,
+  calculateSpentByCategory,
+  calculateSavingGoalProgress,
+} from "../services/financeService";
+
+export const useFinanceData = () => {
+  const { transactions, budgets, savingGoals, accounts } = useFinanceStore();
+
+  const totalBalance = calculateTotalBalance(accounts);
+
+  const spentByCategory = (category: string) => {
+    return calculateSpentByCategory(transactions, category);
+  };
+
+  const savingGoalProgress = (id: string) => {
+    const goal = savingGoals.find((goal) => goal.id === id);
+    return goal ? calculateSavingGoalProgress(goal) : 0;
+  };
+
+  return {
+    totalBalance,
+    spentByCategory,
+    savingGoalProgress,
+    transactions,
+    budgets,
+    savingGoals,
+    accounts,
+  };
+};
