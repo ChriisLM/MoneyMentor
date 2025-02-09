2476117a42d9f1bba73f03b7732365c2502a8fb7
ChriisLM <christianlezcano878@gmail.com>
2025-02-08 22:44:39 -0500

    style: Add styles for background in dark mode

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -5,10 +5,10 @@ import { SideBar } from "../components/SideBar";
 export function Layout() {
   return (
     <main className="h-screen flex flex-col">
-      <Header />
       <section className="flex flex-grow overflow-hidden">
         <SideBar />
-        <div className="flex-grow overflow-y-auto">
+        <div className="flex-grow overflow-y-auto bg-[#f5f5ea] dark:bg-zinc-800">
+          <Header />
           <Outlet />
         </div>
       </section>
