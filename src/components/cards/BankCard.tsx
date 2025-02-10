26516a2def8ec47823c1d8a5e05ef10abbfebbf3
ChriisLM <christianlezcano878@gmail.com>
2025-02-09 22:28:12 -0500

    feat: Replacement of the displayed data

--- src/components/cards/BankCard.tsx
+++ src/components/cards/BankCard.tsx
@@ -1,8 +1,11 @@
+import { Account } from "../../types";
+
 interface BankCardProps {
   styleType: "visa" | "mastercard";
+  cardInfo: Account;
 }
 
-export function BankCard({ styleType }: BankCardProps) {
+export function BankCard({ styleType, cardInfo }: BankCardProps) {
   const cardStyles: Record<"visa" | "mastercard", string> = {
     visa: "bg-blue-600 text-white h-44 p-6 rounded-lg shadow-md relative",
     mastercard: "bg-black text-white h-44 p-6 rounded-lg shadow-md relative",
@@ -13,8 +16,8 @@ export function BankCard({ styleType }: BankCardProps) {
       <h2 className="text-lg font-semibold mb-3">
         {styleType === "visa" ? "Visa Card" : "MasterCard"}
       </h2>
-      <p className="text-4xl pt-1 font-bold">$450</p>
-      <p className="absolute bottom-5 left-6 text-xs">Nombre</p>
+      <p className="text-4xl pt-1 font-bold">${cardInfo.balance.toFixed(2)}</p>
+      <p className="absolute bottom-5 left-6 text-xs">{cardInfo.name}</p>
       {styleType === "visa" && (
         <div className="absolute top-2 right-4 text-2xl font-bold">VISA</div>
       )}
