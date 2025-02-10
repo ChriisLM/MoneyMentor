4e3a21764a4b7e2f4eedd75a061782aaa7879d3c
ChriisLM <christianlezcano878@gmail.com>
2025-02-09 22:20:02 -0500

    style: Added dark mode styles

--- src/components/ButtonAdd.tsx
+++ src/components/ButtonAdd.tsx
@@ -6,7 +6,7 @@ interface ButtonAddProps {
 }
 export function ButtonAdd({ icon = true, children }: ButtonAddProps) {
   return (
-    <button className="flex gap-2 bg-gray-900 text-zinc-50 text-sm px-3 py-2 rounded-lg font-medium hover:bg-gray-800">
+    <button className="flex gap-2 bg-slate-600 shadow-md transition-all text-zinc-50 text-sm px-3 py-2 rounded-lg font-medium hover:bg-gray-800">
       {icon && <AddCircleIcon className="w-5 h-5" />}
       {children}
     </button>
