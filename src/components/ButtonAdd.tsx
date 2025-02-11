9dddfbae23dd19e08bea778a4fdb2e111716a03c
ChriisLM <christianlezcano878@gmail.com>
2025-02-10 20:46:02 -0500

    feat: Added prop onclick

--- src/components/ButtonAdd.tsx
+++ src/components/ButtonAdd.tsx
@@ -3,10 +3,14 @@ import { AddCircleIcon } from "../icons/Icons";
 interface ButtonAddProps {
   icon: boolean;
   children: React.ReactNode;
+  onClick?: () => void;
 }
-export function ButtonAdd({ icon = true, children }: ButtonAddProps) {
+export function ButtonAdd({ icon = true, children, onClick }: ButtonAddProps) {
   return (
-    <button className="flex gap-2 bg-slate-600 shadow-md transition-all text-zinc-50 text-sm px-3 py-2 rounded-lg font-medium hover:bg-gray-800">
+    <button
+      className="flex gap-2 bg-slate-600 shadow-md transition-all text-zinc-50 text-sm px-3 py-2 rounded-lg font-medium hover:bg-gray-800"
+      onClick={onClick}
+    >
       {icon && <AddCircleIcon className="w-5 h-5" />}
       {children}
     </button>
