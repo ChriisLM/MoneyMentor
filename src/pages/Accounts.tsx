9e169498312b9a84a9cc2e015961ae1211d64c8e
ChriisLM <christianlezcano878@gmail.com>
2025-02-10 20:54:22 -0500

    feat: Implemented account data fetching and display from global Zustand store

--- src/pages/Accounts.tsx
+++ src/pages/Accounts.tsx
@@ -4,19 +4,24 @@ import { EmergencyCard } from "../components/cards/EmergencyCard";
 import { SavingAccountCard } from "../components/cards/SavingAccountCard";
 import { CardsViewer } from "../components/CardsViewer";
 import { Title } from "../components/Title";
+import { useFinanceData } from "../hooks/useFinanceData";
 
 export function Accounts() {
+  const { accounts } = useFinanceData();
+
   return (
-    <section className="px-8 py-4">
+    <section className="px-8 pb-4">
       <Title title="Accounts" />
       <div className="grid gap-8 md:grid-cols-2 py-6 px-2">
-        <AccountCard />
-        <BankCardList />
+        <AccountCard accounts={accounts} />
+        <BankCardList accounts={accounts} />
       </div>
-      <EmergencyCard/>
+      <div className="px-2">
+        <EmergencyCard />
+      </div>
       <div className="grid gap-8 md:grid-cols-2 py-6 px-2 relative overflow-hidden">
-        <SavingAccountCard/>
-        <CardsViewer/>
+        <SavingAccountCard />
+        <CardsViewer />
       </div>
     </section>
   );
