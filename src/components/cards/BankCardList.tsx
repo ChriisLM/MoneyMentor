d2c45b7e2a3e26bc197e1fbd92eeba99acd113c5
ChriisLM <christianlezcano878@gmail.com>
2025-02-10 20:48:33 -0500

    refactor: Refactory structure of component and styles

--- src/components/cards/BankCardList.tsx
+++ src/components/cards/BankCardList.tsx
@@ -1,58 +1,61 @@
-import { AddCircleIcon } from "../../icons/Icons";
+import { useState } from "react";
+import { useConvertCurrency } from "../../hooks/useConvertCurrency";
+import { type Account } from "../../types";
+import { ButtonAdd } from "../ButtonAdd";
+import { FormAddDialog } from "../FormAddDialog";
+import { useFinanceData } from "../../hooks/useFinanceData";
 
-const cards = [{ id: "1", name: "Credit Card", balance: -1000, limit: 5000 },{id:"2", name: "Debit Card", balance: -200, limit: 4000}];
-
-const currency = { label: "USD", value: "usd", symbol: "$" };
-
-const exchangeRates = {
-  usd: 1,
-  eur: 0.85,
-  gbp: 0.73,
-  jpy: 110.14,
-  cad: 1.25,
-};
-
-function convertCurrency(amount: number, from: string, to: string): number {
-  return (
-    amount *
-    (exchangeRates[to as keyof typeof exchangeRates] /
-      exchangeRates[from as keyof typeof exchangeRates])
-  );
+interface AccountCardProps {
+  accounts: Account[];
 }
 
-export function BankCardList() {
+export function BankCardList({ accounts }: AccountCardProps) {
+  const { addAccount } = useFinanceData();
+  const [open, setOpen] = useState(false);
+
+  const currency = { label: "USD", value: "usd", symbol: "$" };
+  const filteredAccountsCards = accounts.filter(
+    (account) => account.type === "card"
+  );
+
+  const handleAddAccount = (data: Account) => {
+    addAccount(data);
+  };
+
   return (
-    <div className="rounded-lg border bg-card text-card-foreground shadow">
+    <div className="bg-white rounded-lg shadow border bg-card text-card-foreground dark:bg-neutral-700 dark:border-neutral-500 dark:shadow-neutral-500">
       <div className="flex flex-row items-center justify-between pb-2 space-y-1.5 p-6">
-        <h4 className="font-bold leading-none tracking-tight text-xl">
+        <h4 className="font-bold leading-none tracking-tight text-xl text-gray-800 dark:text-gray-200">
           Cards
         </h4>
-        <button className="flex gap-2 bg-gray-900 text-zinc-50 text-sm px-3 py-2 rounded-lg font-medium hover:bg-gray-800">
-          <AddCircleIcon className="w-5 h-5" />
+        <ButtonAdd icon onClick={() => setOpen(true)}>
           Add Card
-        </button>
+        </ButtonAdd>
+        <FormAddDialog
+          type="card"
+          open={open}
+          onOpenChange={setOpen}
+          onSubmit={(data) => handleAddAccount(data)}
+        />
       </div>
       <div className="flex flex-col p-6 pt-0">
         <ul className="space-y-2">
-          {cards.map((card) => (
-            <li key={card.id} className="flex justify-between items-center">
-              <span>{card.name}</span>
-              <div className="text-right">
-                <div className="font-semibold">
-                  {currency.symbol}
-                  {convertCurrency(card.balance, "usd", currency.value).toFixed(
-                    2
-                  )}
-                </div>
-                <div className="text-sm text-muted-foreground text-gray-500">
-                  Limit: {currency.symbol}
-                  {convertCurrency(card.limit, "usd", currency.value).toFixed(
-                    2
-                  )}
-                </div>
-              </div>
-            </li>
-          ))}
+          {filteredAccountsCards.map((card) => {
+            const { formattedValue } = useConvertCurrency(
+              card.balance,
+              currency
+            );
+            return (
+              <li key={card.id} className="flex justify-between items-center">
+                <span className="text-gray-600 dark:text-gray-200">
+                  {card.name}
+                </span>
+                <span className="font-semibold dark:text-neutral-400">
+                  {formattedValue}
+                </span>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
