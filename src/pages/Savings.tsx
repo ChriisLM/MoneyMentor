20e9f5eac9df903f1eac971fb0f3951b88ed2ff0
ChriisLM <christianlezcano878@gmail.com>
2025-02-10 20:55:30 -0500

    feat: Implemented saving goal data fetching and display from global Zustand store

--- src/pages/Savings.tsx
+++ src/pages/Savings.tsx
@@ -1,35 +1,37 @@
+import { useState } from "react";
 import { ButtonAdd } from "../components/ButtonAdd";
 import { SavingGoalCard } from "../components/cards/SavingGoalCard";
 import { Title } from "../components/Title";
+import { useFinanceData } from "../hooks/useFinanceData";
+import { FormAddDialog } from "../components/FormAddDialog";
+import { type SavingGoal } from "../types";
 
 export function Savings() {
+  const { savingGoals, addSavingGoal } = useFinanceData();
+  const [open, setOpen] = useState(false);
+
+  const handleAddGoal = (data: SavingGoal) => {
+    addSavingGoal(data);
+  };
+
   return (
-    <section className="px-8 py-4">
+    <section className="px-8 pb-4">
       <div className="flex flex-row items-center justify-between pr-2">
         <Title title="Saving Goals" />
-        <ButtonAdd icon={true}>Add Goal</ButtonAdd>
+        <ButtonAdd icon={true} onClick={() => setOpen(true)}>
+          Add Goal
+        </ButtonAdd>
+        <FormAddDialog
+          type="goal"
+          open={open}
+          onOpenChange={setOpen}
+          onSubmit={(data) => handleAddGoal(data)}
+        />
       </div>
       <div className="grid gap-4 lg:grid-cols-2 py-6 px-2">
-        <SavingGoalCard
-          goalName="Vacation Fund"
-          saved={1500}
-          target={5000}
-          color="bg-blue-500"
-        />
-
-        <SavingGoalCard
-          goalName="New Laptop"
-          saved={800}
-          target={1500}
-          color="bg-green-500"
-        />
-
-        <SavingGoalCard
-          goalName="Emergency Fund"
-          saved={3000}
-          target={10000}
-          color="bg-red-500"
-        />
+        {savingGoals.map((goal) => (
+          <SavingGoalCard key={goal.id} goal={goal} />
+        ))}
       </div>
     </section>
   );
