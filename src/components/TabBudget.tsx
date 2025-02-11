de118f36813f95aa013fc6bc2d63ff4f9a301864
ChriisLM <christianlezcano878@gmail.com>
2025-02-10 20:47:37 -0500

    style: Added dark mode styles

--- src/components/TabBudget.tsx
+++ src/components/TabBudget.tsx
@@ -1,16 +1,20 @@
 import { useState } from "react";
 import { BudgetCard } from "./cards/BudgetCard";
+import { useFinanceData } from "../hooks/useFinanceData";
+import { calculateBudgetTotal } from "../utils/calculateBudgetTotal";
 
 export function TabBudget() {
   const [activeTab, setActiveTab] = useState("monthly");
+  const { budgets } = useFinanceData();
+  const { totalSpent, totalLimit } = calculateBudgetTotal(budgets);
   return (
     <div className="flex flex-col md:flex-row py-4">
-      <div className="flex flex-col justify-center md:w-1/4 border-b md:border-b-0 md:border-r border-gray-200 md:pr-6 pb-4 md:pb-0 mx-6 md:mx-0">
+      <div className="flex flex-col justify-center md:w-1/4 border-b md:border-b-0 md:border-r border-gray-300 md:pr-6 pb-4 md:pb-0 mx-6 md:mx-0">
         <button
           className={`block w-full text-center p-2 rounded-lg mb-2 font-medium ${
             activeTab === "monthly"
-              ? "bg-gray-100 text-blue-500"
-              : "text-gray-700"
+              ? "bg-gray-300 text-blue-500 dark:text-blue-400 dark:bg-zinc-700 "
+              : "text-gray-700 dark:text-gray-200 hover:bg-gray-300 hover:dark:bg-zinc-700"
           }`}
           onClick={() => setActiveTab("monthly")}
         >
@@ -19,8 +23,8 @@ export function TabBudget() {
         <button
           className={`block w-full text-center p-2 rounded-lg font-medium ${
             activeTab === "annual"
-              ? "bg-gray-100 text-blue-500"
-              : "text-gray-700"
+              ? "bg-gray-300 text-blue-500 dark:text-blue-400 dark:bg-zinc-700 "
+              : "text-gray-700 dark:text-gray-200 hover:bg-gray-300 hover:dark:bg-zinc-700"
           }`}
           onClick={() => setActiveTab("annual")}
         >
@@ -29,9 +33,19 @@ export function TabBudget() {
       </div>
       <div className="md:w-3/4 w-full px-6 md:pr-2 pt-4 md:pt-0">
         {activeTab === "monthly" ? (
-          <BudgetCard title="Monthly Budget" spent={1500} total={2000} />
+          <BudgetCard
+            title="Monthly Budget"
+            spent={totalSpent}
+            total={totalLimit}
+            isCommon={false}
+          />
         ) : (
-          <BudgetCard title="Annual Budget" spent={18000} total={24000} />
+          <BudgetCard
+            title="Annual Budget"
+            spent={totalSpent * 12}
+            total={totalLimit * 12}
+            isCommon={false}
+          />
         )}
       </div>
     </div>
